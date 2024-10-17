c5714a17981d60d28cd6c5d7aae6994496212584
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2024-10-17 17:27:42 -0300

    Create Ex07.cpp

---
+++ Exercicios/Ex07.cpp
@@ -0,0 +1,34 @@
+//Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (void)
+ {
+
+float A, B, C, D, R;
+
+cout << "Digite o valor da primeira nota" << endl;;
+cin >> A;
+cin.ignore(80, '\n'); //Limpa o buffer do teclado
+
+cout << "Digite o valor da segunda nota" << endl;;
+cin >> B;
+cin.ignore(80, '\n'); //Limpa o buffer do teclado
+
+cout << "Digite o valor da terceira nota" << endl;;
+cin >> C;
+cin.ignore(80, '\n'); //Limpa o buffer do teclado
+
+cout << "Digite o valor da quarta nota" << endl;;
+cin >> D;
+cin.ignore(80, '\n'); //Limpa o buffer do teclado
+
+    R = (A + B + C + D) / 4;
+
+cout << "A media das notas eh " << R << '\n';
+
+return 0;
+
+}
