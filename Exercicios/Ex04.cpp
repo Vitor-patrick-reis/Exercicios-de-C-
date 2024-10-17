2f11ded4190f68a1506e02eed95361a44c3c34e7
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2024-10-17 17:23:31 -0300

    Create Ex04.cpp

---
+++ Exercicios/Ex04.cpp
@@ -0,0 +1,20 @@
+// Faça um Programa que peça dois números e imprima a Subtração dos valores.
+
+#include <iostream>
+ using namespace std;
+
+ int main (void)
+ {
+ int A, B, R;
+
+ cin >> A;
+ cin >> B;
+
+    R = A - B;
+
+ cout << "O resultado de "  << A << " - " << B <<" = " << R;
+
+ return 0;
+
+
+ }
