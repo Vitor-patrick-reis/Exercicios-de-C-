9ca6370aa9c1d5878244722c755c0ac6195e2537
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2024-10-17 17:20:23 -0300

    Create Ex02.cpp

---
+++ Exercicios/Ex02.cpp
@@ -0,0 +1,18 @@
+//Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+#include <iostream>
+using namespace std;
+
+int main (void)
+ {
+
+  int A;
+
+     cout << "Digite um numero" <<endl;
+     cin >> A;
+
+    cout << "Seu numero e " << A;
+
+    return 0;
+
+}
