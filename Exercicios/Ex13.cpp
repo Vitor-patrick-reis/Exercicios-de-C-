62038cb6bbadd4f0a6bf851d90b94adf4e2bdf64
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2024-10-19 14:28:15 -0300

    Create Ex13.cpp

---
+++ Exercicios/Ex13.cpp
@@ -0,0 +1,38 @@
+/*
+Tendo como dado de entrada a altura (h) de uma pessoa,
+Pergunte o seu sexo, e construa um algoritmo que calcule seu peso ideal,
+utilizando as seguintes fórmulas:
+Para homens: (72.7*h) - 58
+Para mulheres: (62.1*h) - 44.7
+*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main (void)
+{
+  float H, P;
+  char resp {'m' or 'h'};
+
+  cout <<"Qual a sua altura" << endl;
+  cin >> H;
+
+  cout <<"Qual seu sexo" <<endl;
+  cin >> resp;
+
+    if(resp == 'h'){
+        P = (72.7*H) - 58;
+
+    cout <<"Seu peso ideal eh " << P <<endl;
+    }
+
+    else{
+        P = (62.1*H) - 44.7;
+
+     cout <<"Seu peso ideal eh " << P <<endl;
+        }
+
+return 0;
+}
+
