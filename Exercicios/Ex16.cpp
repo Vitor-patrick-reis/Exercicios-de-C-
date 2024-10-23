b22e7c1226ad7f83bec8664e39923794510269fc
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2024-10-23 17:12:00 -0300

    Create Ex16.cpp

---
+++ Exercicios/Ex16.cpp
@@ -0,0 +1,23 @@
+//Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+ #include<iostream>
+ #include<iomanip>
+ using namespace std;
+
+ int main(void)
+ {
+ int A;
+
+ cout<<"Digite o primeiro numero"<<endl;
+ cin>>A;
+
+    if(A > 0)
+
+    cout<< "O valor digitado eh positivo" ;
+
+      else
+       cout<< "O valor digitado eh negativo " ;
+
+ return 0;
+
+ }
