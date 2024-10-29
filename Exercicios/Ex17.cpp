07968493e2a068dc77b8f5bd6aa0d5300ebe8cfe
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2024-10-29 14:14:57 -0300

    Update Ex17.cpp

--- Exercicios/Ex17.cpp
+++ Exercicios/Ex17.cpp
@@ -1,3 +1,7 @@
+/*Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+*/
+
 #include<iostream>
  #include<iomanip>
  using namespace std;
