33061489ea7cb0897a961d550e069f3d3f0176a9
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2024-11-27 12:39:02 -0300

    Create Ex31.cpp

---
+++ Exercicios/Ex31.cpp
@@ -0,0 +1,23 @@
+//Crie uma função que receba dois números inteiros e retorne o maior deles.
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+int A, B;
+
+    cout<<"Entre o primeiro valor"<<endl;
+    cin>>A;
+
+    cout<<"Entre o primeiro valor"<<endl;
+    cin>>B;
+
+if(A > B)
+    cout<<A;
+
+else
+cout<<" O maior numero " << B;
+
+return 0;
+}
