58b79cea0b09f7d8436b54ddea54f49f8020e92e
Vitor Patrick <164456236+Vitor-patrick-reis@users.noreply.github.com>
2024-11-27 11:36:11 -0300

    Create Ex25.cpp

---
+++ Exercicios/Ex25.cpp
@@ -0,0 +1,21 @@
+//Escreva um programa que leia dois números inteiros e exiba a soma, subtração, multiplicação e divisão deles.
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+int A, B;
+
+cin>> A;
+cin>> B;
+
+cout <<"Soma " << A + B <<endl;
+cout <<"Subtracao " << A - B <<endl;
+cout <<"Multiplicao " << A * B <<endl;
+cout <<"Divisao " << A / B <<endl;
+
+
+return 0;
+
+}
